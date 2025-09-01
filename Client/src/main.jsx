987818fc7cc74329843a9e93289183c9f22acfd9
harshchandel2005 <harshchandel12310@gmail.com>
2025-09-02 00:13:44 +0530

    Resolve merge conflict in main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,21 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
-import App from './App.jsx'
-=======
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import './index.css'
-import Layout from './Layout.jsx'
-
-const router=createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='' element={<Layout/>}>
-      {/* <Route path='/' */}
-    </Route>
-  )
-)
->>>>>>> upstream/main
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
