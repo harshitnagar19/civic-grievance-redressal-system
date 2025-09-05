636fe754683753639f68e9ae517027dbb60687b6
Hemant Kamliya <hemantkamliya87@gmail.com>
2025-09-05 12:15:59 +0530

    stash

--- Client/src/Layout.jsx
+++ Client/src/Layout.jsx
@@ -4,7 +4,6 @@ import {Header} from './Components/Header/Header'
 import {Footer} from './Components/Footer/Footer'
 function Layout() {
   const [count, setCount] = useState(0)
-
   return (
    <>
     <Header/>
