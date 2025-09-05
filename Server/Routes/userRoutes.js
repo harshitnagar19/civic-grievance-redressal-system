4a386011963658e4a6cf4f622d034d292d6e81bf
harshit nagar <nharshit7024@gmail.com>
2025-09-05 21:10:07 +0530

    header footer remove from signup page

--- Server/Routes/userRoutes.js
+++ Server/Routes/userRoutes.js
@@ -4,5 +4,6 @@ const route = express.Router()
 
 import userControllers from "../Controllers/userControllers.js"
 route.post("/signup",userControllers.signup)
+route.post("/login",userControllers.login)
 
 export default route; 
