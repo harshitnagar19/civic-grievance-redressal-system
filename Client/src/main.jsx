cd938cecf2d9769ff13715d90466f558ae965778
Hemant Kamliya <hemantkamliya87@gmail.com>
2025-09-06 15:54:22 +0530

    Added loginWrapper form

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -7,11 +7,15 @@ import Layout from './Layout.jsx'
 import UserSignUpWrapper from './Components/UserSignUp/UserSignUpWrapper.jsx'
 import { Provider } from 'react-redux'
 import { Store } from './Store/Store.js'
+import UserLoginWrapper from './Components/Login/UserLoginWrapper.jsx';
+import UserDashboard from './Components/Dashboard/UserDashboard.jsx';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path='/user-signup' element={<UserSignUpWrapper></UserSignUpWrapper>}></Route>
+      <Route path='/user-signup' element={<UserSignUpWrapper />}></Route>
+      <Route path='/user-login' element={<UserLoginWrapper />}></Route>
       <Route path='' element={<Layout />}>
+        <Route path='/user-dashboard' element={<UserDashboard />}></Route>
       </Route>
     </>
   )
