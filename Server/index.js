df41370540e11df7f9733ceac4d4c3c29c3f07e2
harshit nagar <nharshit7024@gmail.com>
2025-09-08 21:10:36 +0530

    create user refresh Api

--- Server/index.js
+++ Server/index.js
@@ -1,23 +1,24 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.js";
-import bodyParser from "body-parser"
-const app = express()
-dotenv.config()
-app.use(cors())
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+import bodyParser from "body-parser";
+import { v2 as cloudinary } from "cloudinary";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-import route from "./Routes/userRoutes.js"
-app.use("/user",route)
+import route from "./Routes/userRoutes.js";
+app.use("/user", route);
 
-const PORT  = process.env.PORT
-connectDB().then(
-    ()=>{
-        app.listen(PORT, ()=>{
-        console.log(`Server is running on port : ${PORT}`)
-        })
-    }
-)
+const PORT = process.env.PORT;
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port : ${PORT}`);
+  });
+});
--- Server/utils/password/verifyPassword.js
+++ Server/utils/password/verifyPassword.js
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 const verifyPassword = async (password, hashedPassword) => {
   try {
     const isMatch = await bcrypt.compare(password, hashedPassword);
+    console.log(isMatch)
     return isMatch;
   } catch (err) {
     throw new Error("Error while verifying password: " + err.message);
