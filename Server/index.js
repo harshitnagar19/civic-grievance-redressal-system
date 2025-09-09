fac9b12b31e79e6002c2c6031e857cbb1d985876
harshit gawli <harshitgawli@gmail.com>
2025-09-09 22:51:18 +0530

    department schema updated

--- Server/index.js
+++ Server/index.js
@@ -13,15 +13,10 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-<<<<<<< HEAD
 import route from "./Routes/userRoutes.js"
 import deptRoute from "./Routes/departmentRoutes.js";
 app.use("/user",route)
 app.use("/department",deptRoute)
-=======
-import route from "./Routes/userRoutes.js";
-app.use("/user", route);
->>>>>>> 8b610abdf12a566a9b69b38c82dc1f3c86ed3e60
 
 const PORT = process.env.PORT;
 connectDB().then(() => {
