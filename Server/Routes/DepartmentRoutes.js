3be448e6315223c3260fdb5b831a31afe26ebbdd
harshitnagar19 <127100557+harshitnagar19@users.noreply.github.com>
2025-09-10 12:04:22 +0530

    Update DepartmentRoutes.js

--- Server/Routes/DepartmentRoutes.js
+++ Server/Routes/DepartmentRoutes.js
@@ -1,9 +1,9 @@
 import express from "express"
 import departmentControllers from "../Controllers/departmentControllers.js"
 
-const deptRoute = express.Router()
+const route  = express.Router()
 
-deptRoute.post("/signup",departmentControllers.signup)
-deptRoute.post("/login",departmentControllers.login)
+route .post("/signup",departmentControllers.signup)
+route .post("/login",departmentControllers.login)
 
-export default deptRoute;
+export default route ;
