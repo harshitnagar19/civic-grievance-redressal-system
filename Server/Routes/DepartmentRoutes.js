c4aefbe072562574417fcb086a930b5d53bdc693
harshit gawli <harshitgawli@gmail.com>
2025-09-12 21:32:06 +0530

    department refresh API added

--- Server/Routes/DepartmentRoutes.js
+++ Server/Routes/DepartmentRoutes.js
@@ -1,12 +1,12 @@
 import express from "express"
 import departmentControllers from "../Controllers/departmentControllers.js"
-import { verifyToken } from "../middleware/verifyToken.js"
+import { verifyDepartmentToken } from "../middleware/verifyDeptToken.js"
 
 const deptroute  = express.Router()
 
 deptroute.post("/signup",departmentControllers.signup)
 deptroute.post("/login",departmentControllers.login)
-deptroute.post("/refresh",verifyToken,departmentControllers.refresh)
+deptroute.post("/refresh",verifyDepartmentToken,departmentControllers.refresh)
 
 
 export default deptroute ;
