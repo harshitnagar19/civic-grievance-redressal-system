dccf833eac5402bf0e31a81b225c1db431c612a9
harshit nagar <nharshit7024@gmail.com>
2025-09-12 23:26:08 +0530

    add gitingore file

---
+++ Server/Routes/complainRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import complainController from "../Controllers/complainControllers.js";
+
+const complainRoute = express.Router();
+
+complainRoute.post("/add",complainController.add);
+
+export default complainRoute;
