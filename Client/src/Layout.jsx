22cca0ab0e4d86967c0090174830d0dd7d23d7af
Hemant Kamliya <hemantkamliya87@gmail.com>
2025-09-30 17:22:30 +0530

    revert changes

--- Client/src/Layout.jsx
+++ Client/src/Layout.jsx
@@ -1,17 +1,35 @@
-import { Outlet } from "react-router-dom";
-import Header from "./Components/Header/Header";
-import Footer from "./Components/Footer/Footer";
+import { Outlet } from 'react-router-dom'
+import Footer from './Components/Footer/Footer'
+import Header from './Components/Header/Header'
+import OAuth from './Components/auth/OAuth'
+import { useSelector, useDispatch } from "react-redux";
+import UserSidebarNavigation from './Components/SidebarNavigation/UserSidebarNavigation'
+import DeptSidebarNavigation from './Components/SidebarNavigation/DeptSidebarNavigation'
+function Layout() {
 
-const Layout = () => {
+  const userData = useSelector((store) => store.userData);
+  const deptData = useSelector((store) => store.departmentData);
+  const userRole = userData?.role;
+  const deptRole = deptData?.role;
   return (
     <>
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      {userRole !== "" || deptRole !== "" ?
+        userRole !== "" ?
+          <div className={'flex gap-7'}>
+           <OAuth> <UserSidebarNavigation /></OAuth>
+            <Outlet />
+          </div>
+          :
+          <div className={'flex gap-7'}>
+           <OAuth> <DeptSidebarNavigation /></OAuth>
+            <Outlet />
+          </div>
+        : ''
+      }
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
