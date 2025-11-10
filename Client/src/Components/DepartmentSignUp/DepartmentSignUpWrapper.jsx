95313941b1f1adc5cfca4356e7c7ebf6cac447f4
harshitnagar19 <127100557+harshitnagar19@users.noreply.github.com>
2025-11-10 10:54:12 +0530

    Revert "Created department signup page"

--- Client/src/Components/DepartmentSignUp/DepartmentSignUpWrapper.jsx
+++ Client/src/Components/DepartmentSignUp/DepartmentSignUpWrapper.jsx
@@ -1,68 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Formik, Form } from "formik";
-import axios from "axios";
-import { toast } from "react-toastify";
-import { DepartmentSignUpValidation } from "../../validations/DepartmentValidation";
+import { useDispatch, useSelector } from 'react-redux'
+import { DepartmentSignUpValidation } from "../../validations/DepartmentValidation"
 import DepartmentSignUpForm from "./DepartmentSignUpForm";
+import axios from 'axios'
+import { toast } from 'react-toastify';
 import { useNavigate } from "react-router-dom";
-import { routes } from "../../data/routes";
-
+import { routes } from '../../data/routes'
 function DepartmentSignUpWrapper() {
-  const navigate=useNavigate()
-  const initialValues = {
-    departmentName: "",
-    headOfDepartment: "",
-    departmentShortName: "",
-    email: "",
-    password: "",
-    mobileNumber: "",
-    city: "",
-    state: "",
-    deptAddress: "",
-    district: "",
-  };
-
-  const handleSubmit = async (values, { setSubmitting, resetForm }) => {
-    
-    const { district, departmentName, headOfDepartment, departmentShortName, ...restValues } = values;
-    
-    const apiPayload = {
-      DepartmentName:departmentName,
-      DepartmentShortName: departmentShortName,
-      HeadOfDepartment: headOfDepartment,
-      ...restValues 
-    };
-    console.log(apiPayload)
-    try {
-      const response = await axios.post(
-        `${import.meta.env.VITE_BASEURL}/department/signup`,
-        apiPayload
-      );
-      toast.success("Department registered successfully!");
-      resetForm();
-      navigate(routes.deptLogin)
-    } catch (error) {
-      console.error(error);
-      toast.error(error.response.data.msg)
-    } finally {
-      setSubmitting(false);
+    const initialValues = {
+        departmentName: "",
+        headOfDepartment: "",
+        departmentShortName: "",
+        email: "",
+        password: "",
+        mobileNumber: "",
+        city: "",
+        state: "",
+        deptAddress: "",
+        district:''
     }
-  };
+    const handleSubmit = () => {
 
-  return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={DepartmentSignUpValidation}
-      validateOnChange={false}
-      onSubmit={handleSubmit}
-    >
-      {(formikProps) => (
-        <Form>
-          <DepartmentSignUpForm formikProps={formikProps} />
-        </Form>
-      )}
-    </Formik>
-  );
+    }
+    return (
+        <>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={DepartmentSignUpValidation}
+                validateOnChange={false}
+                onSubmit={handleSubmit}
+            >
+                {(formikProps) => (
+                    <Form>
+                        <DepartmentSignUpForm
+                            formikProps={formikProps}
+                        />
+                    </Form>
+                )
+
+                }
+
+            </Formik>
+        </>
+    )
 }
 
-export default DepartmentSignUpWrapper;
+export default DepartmentSignUpWrapper
